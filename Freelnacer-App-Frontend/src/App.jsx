5dfc519d94ab254217f71285c65be7b86805d4c8
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-01-26 10:39:35 -0600

    Added QueryClient

--- Freelnacer-App-Frontend/src/App.jsx
+++ Freelnacer-App-Frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import Auth from "./pages/Auth"
+import { Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Auth from "./pages/Auth";
+
+const queryClient = new QueryClient();
 
 function App() {
   
   return (
-    <div className="container xl:max-w-screen-xl">
-      <Routes>
-        <Route path="/auth" element={<Auth/>}>
-        </Route>
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="container xl:max-w-screen-xl">
+        <Routes>
+          <Route path="/auth" element={<Auth/>}>
+          </Route>
+        </Routes>
+      </div>
+    </QueryClientProvider>
   )
 }
 
