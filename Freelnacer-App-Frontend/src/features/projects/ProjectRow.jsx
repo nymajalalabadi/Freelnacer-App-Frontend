b959c812641115543711bb289e9af7d314070200
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-02-02 10:05:01 -0600

    Added Modal Componenet

--- Freelnacer-App-Frontend/src/features/projects/ProjectRow.jsx
+++ Freelnacer-App-Frontend/src/features/projects/ProjectRow.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+import Modal from '../../ui/Modal';
 import Table from '../../ui/Table'
 import toLocalDateShort from '../../utils/toLocalDateShort'
 import { toNumbersWithComma } from '../../utils/toNumbers'
 import truncateText from '../../utils/truncateText'
+import { HiEye, HiOutlineTrash } from "react-icons/hi";
+import { TbPencilMinus } from "react-icons/tb";
 
 function ProjectRow({project, index}) {
+
+    const [isEditOpen, setIsEditOpen] = useState(false);    
+
   return (
     <Table.Row>
         <td>{index + 1}</td>
@@ -22,7 +29,17 @@ function ProjectRow({project, index}) {
         <td>
             {project.status === "OPEN" ? ( <span className="badge badge--success">OPEN</span> ) : (<span className="badge badge--danger">CLOSED</span>)}
         </td>
-        <td>...</td>
+        <td>
+            <div className="flex items-center gap-x-4">
+                <button onClick={() => setIsEditOpen(true)}>
+                    <TbPencilMinus className="w-5 h-5 text-primary-900" />
+                </button>
+                <Modal open={isEditOpen} title="modal title" onClose={() => setIsEditOpen(false)}>this is modal ....</Modal>
+                <button>
+                    <HiOutlineTrash className="w-5 h-5 text-error" />
+                </button>
+            </div>
+        </td>
     </Table.Row>
   )
 }
