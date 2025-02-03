68fc0031e9a704e4a5130b232d2ae66d3f5c2691
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-02-03 11:23:29 -0600

    Added Delete Project Function

--- Freelnacer-App-Frontend/src/features/projects/ProjectRow.jsx
+++ Freelnacer-App-Frontend/src/features/projects/ProjectRow.jsx
@@ -7,11 +7,13 @@ import truncateText from '../../utils/truncateText'
 import { HiEye, HiOutlineTrash } from "react-icons/hi";
 import { TbPencilMinus } from "react-icons/tb";
 import ConfirmDelete from '../../ui/ConfirmDelete';
+import useRemoveProject from './useRemoveProject';
 
 function ProjectRow({project, index}) {
 
     const [isEditOpen, setIsEditOpen] = useState(false);    
     const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+    const {removeProject, isDeleting} = useRemoveProject();
 
   return (
     <Table.Row>
@@ -41,8 +43,12 @@ function ProjectRow({project, index}) {
                     <HiOutlineTrash className="w-5 h-5 text-error" />
                 </button>
                 <Modal open={isDeleteOpen} title={`Delete ${project.title}`} onClose={() => setIsDeleteOpen(false)}>
-                    <ConfirmDelete resourceName={project.title} onClose={() => setIsDeleteOpen(false)} onConfirm={() => {}} disabled={false}/>
-                </Modal>vvv
+                    <ConfirmDelete resourceName={project.title} onClose={() => setIsDeleteOpen(false)} 
+                        onConfirm={() => removeProject(project._id, {
+                        onSuccess: () => setIsDeleteOpen(false),
+                        onError: () => setIsDeleteOpen(false)
+                    })} disabled={false} />
+                </Modal>
             </div>
         </td>
     </Table.Row>
--- Freelnacer-App-Frontend/src/features/projects/useOwnerProjects.js
+++ Freelnacer-App-Frontend/src/features/projects/useOwnerProjects.js
@@ -2,8 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getOwnerProjectApi } from "../../services/projectService";
 
 
-export default function useOwnerProjects() 
-{
+export default function useOwnerProjects() {
   const { data, isLoading } = useQuery ({
     queryKey: ["owner-projects"],
     queryFn: getOwnerProjectApi,
