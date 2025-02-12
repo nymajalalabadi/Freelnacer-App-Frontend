4447d6688d1ce6a36bb3db8768cc9af2a64cf2e3
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-02-12 15:53:12 -0600

    Added Window.MatchMedia

--- Freelnacer-App-Frontend/src/context/DarkModeContext.js
+++ Freelnacer-App-Frontend/src/context/DarkModeContext.js
@@ -6,22 +6,24 @@ const DarkModeContext = createContext();
 
 export function DarkModeProvider({ Children }) {
 
-  const [ isDarkMode, setIsDarkMode ] = useLocalStorageState("isDarkMode", false)
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    "isDarkMoode",
+    window.matchMedia("(prefers-color-scheme: dark)").matches // true, false
+  );
 
-  const toggleDarkMode = ()  => setIsDarkMode((prev) => !prev);
+  const toggleDarkMode = () => setIsDarkMode((prev) => !prev);
 
   useEffect(() => {
-      if(isDarkMode)
-      {
-        document.documentElement.classList.add("dark-mode");
-        document.documentElement.classList.remove("light-mode");
-      }
-      else
-      {
-        document.documentElement.classList.add("light-mode");
-        document.documentElement.classList.remove("dark-mode");
-      }
-  }, [ isDarkMode ])
+    if (isDarkMode) 
+    {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else 
+    {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
 
   return (
     <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
