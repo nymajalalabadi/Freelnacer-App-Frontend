71da01b345717bbeaced928c594b2bbd1f41e5e7
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-02-19 11:40:47 -0600

    main

--- Freelnacer-App-Frontend/src/ui/AppLayout.jsx
+++ Freelnacer-App-Frontend/src/ui/AppLayout.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
 
-
 function AppLayout({ children }) {
   return (
     <div className="grid h-screen grid-rows-[auto_1fr] grid-cols-[15rem_1fr]">
         <Header/>
-        {children}
+        { children }
         <div className="bg-secondary-100 p-8 overflow-y-auto">
             <div className="mx-auto max-w-screen-lg flex flex-col gap-y-12">
               <Outlet />
