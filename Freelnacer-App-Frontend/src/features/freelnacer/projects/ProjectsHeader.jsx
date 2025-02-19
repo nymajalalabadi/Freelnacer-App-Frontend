dfbff73895781a590d8e5c17bc78944c77936e15
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-02-19 09:43:41 -0600

    Added Filter Component

--- Freelnacer-App-Frontend/src/features/freelnacer/projects/ProjectsHeader.jsx
+++ Freelnacer-App-Frontend/src/features/freelnacer/projects/ProjectsHeader.jsx
@@ -1,12 +1,28 @@
-import useCategories from "../../../hooks/useCategories"
-import FilterDropDown from "../../../ui/FilterDropDown"
+import useCategories from "../../../hooks/useCategories";
+import Filter from "../../../ui/Filter";
+import FilterDropDown from "../../../ui/FilterDropDown";
+
+const sortOptions = [
+  { value: "latest", label: "Newest" },
+  { value: "earliest", label: "Oldest" }
+];
+
+const statusOptions = [
+  { value: "ALL", label: "All" },
+  { value: "OPEN", label: "Open" },
+  { value: "CLOSED", label: "Closed" }
+];
 
 function ProjectsHeader() {
-  const { transformedCategories } = useCategories()
+
+  const { transformedCategories } = useCategories();
+
   return (
     <div className="flex items-center justify-between text-secondary-700 mb-8">
       <h1 className="text-lg font-bold ">List Of Projects</h1>
       <div>
+      <Filter filterField="status" options={statusOptions}/>
+      <FilterDropDown filterField="sort" options={sortOptions}/>
 
         <FilterDropDown filterField="category" options={[ 
           {
