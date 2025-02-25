0ea415afb9af31612e964bfe884ddcd11f256e51
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-02-24 18:55:41 -0600

    Completed App Component

--- Freelnacer-App-Frontend/src/pages/Proposals.jsx
+++ Freelnacer-App-Frontend/src/pages/Proposals.jsx
@@ -3,7 +3,7 @@ import ProposalsTable from '../features/proposals/ProposalsTable'
 function Proposals() {
   return (
     <div>
-      <h1 className="font-black text-secondary-700 text-xl mb-8">Your Proposals</h1>
+      <h1 className="font-black text-secondary-700 text-xl mb-8">List Of Proposals</h1>
       <ProposalsTable/>
     </div>
   )
